0eb9affc7e93387c42a9cdbd3cff1a759f917aab
Antonio González <antoni.dev.13@gmail.com>
2022-07-26 14:52:29 +0200

    gestionar: Crear tareas y mostrar lista persistente

--- app.js
+++ app.js
@@ -1,20 +1,56 @@
 
 require('colors');
 
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
+const { inquirerMenu,
+        pausa,
+        leerInput
+     } = require('./helpers/inquirer');
+const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
+const { guardarDB,
+        leerDB
+     } = require('./helpers/guardarArchivo')
+// const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 console.clear();
 
 
-const main = async() =>{
-    console.log('Hola mundo');
+const main = async () => {
 
-    // do {
-        mostrarMenu();
-    // } while (opt !== 0);
-   
+    let opt = '';
+    const tareas = new Tareas();
 
-    // pausa();
+    const tareasDB = leerDB();
+
+    if ( tareasDB ) {
+        //Establecer las tareas
+        tareas.cargarTareasFromArray( tareasDB )
+    }
+
+    do {
+        opt = await inquirerMenu();
+        switch (opt) {
+            case '1':
+                // crear opción
+                const desc = await leerInput('Descripción: ');
+                console.log(desc);
+                tareas.crearTarea(desc)
+                break;
+            case '2':
+                console.log(tareas.listadoArr);
+                break;
+        }
+
+        // const tarea = new Tarea('Comprar comida');
+        // tareas._listado[tarea.id] = tarea;
+        // console.log(tareas)
+
+        guardarDB( tareas.listadoArr );
+
+        if (opt !== '0') await pausa();
+
+    } while (opt !== '0');
+
 
 }
 
